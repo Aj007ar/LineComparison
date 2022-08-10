c7640a4360f24be772432d20a5b0571bd001428c
Aj007ar <aj007.ar@gmail.com>
2022-08-10 19:27:21 +0530

    UC1 files added

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("**************WELCOME TO LINE COMPARISON**************");
+            Console.WriteLine("1.UC1 Length Calculation");
+
+            Console.WriteLine("Enter number for execution");
+            int check = Convert.ToInt32(Console.ReadLine());
+
+            switch (check)
+            {
+                case 1:
+                    Console.WriteLine("**************Calculation Length**************");
+
+                    UC1LentghCal uC1 = new UC1LentghCal();
+                    uC1.LineLength();
+                    break;
+            }
+
         }
     }
 }
---
+++ LineComparison/UC1LentghCal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class UC1LentghCal
+    {
+        public void LineLength()
+        {
+            Console.WriteLine("Enter length for x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            
+            double a = Math.Sqrt(Math.Pow(x2 - x1, 2));
+            double b = Math.Sqrt(Math.Pow(y2 - y1, 2));
+            double c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
