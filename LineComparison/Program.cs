720045e339705e9c1983d3a7277a6065e8ee97da
Aj007ar <aj007.ar@gmail.com>
2022-08-10 20:08:55 +0530

    UC2 files added

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("**************WELCOME TO LINE COMPARISON**************");
             Console.WriteLine("1.UC1 Length Calculation");
+            Console.WriteLine("2.UC2 check line equality");
+
 
             Console.WriteLine("Enter number for execution");
             int check = Convert.ToInt32(Console.ReadLine());
@@ -17,6 +19,12 @@
 
                     UC1LentghCal uC1 = new UC1LentghCal();
                     uC1.LineLength();
+                    break;
+                case 2:
+                    Console.WriteLine("**************Line Equality**************");
+
+                    UC2LineEquality equality = new UC2LineEquality();
+                    equality.EqualLines();
                     break;
             }
 
---
+++ LineComparison/UC2LineEquality.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class UC2LineEquality
+    {
+        public void EqualLines()
+        {
+            double firstLength = 0;
+            double secLength = 0;
+            Console.WriteLine("Enter length for x1");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for x2");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for x3");
+            double x3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for x4");
+            double x4 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y1");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y2");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y3");
+            double y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter length for y4");
+            double y4 = Convert.ToDouble(Console.ReadLine());
+
+
+            firstLength = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("first Length is:" + Math.Round(firstLength, 2)); 
+            secLength = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));        
+            Console.WriteLine("Second Length is :" + Math.Round(secLength, 2));   
+
+            Console.WriteLine("\nUsing Equal Method");
+            if (firstLength.Equals(secLength))
+            {
+                Console.WriteLine("Length of the line is equal");
+            }
+            else
+            {
+                Console.WriteLine("length of the line is not equal");
+            }
+        }
+    }
+}
